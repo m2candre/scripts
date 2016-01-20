1d7eabee1fa7105d77bb2235084cd071863c8b58
Marcos Andre <m2c.andre@gmail.com>
2016-01-20 15:24:57 -0200

    Locale Ubuntu
    Locale Ubuntu

---
+++ locale.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo bash -c "echo 'export LANGUAGE=en_US.UTF-8' >> /etc/bash.bashrc"
+sudo bash -c "echo 'export LANG=en_US.UTF-8' >> /etc/bash.bashrc"
+sudo bash -c "echo 'export LC_ALL=en_US.UTF-8' >> /etc/bash.bashrc"
+
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+
+
+#pt_BR.UTF-8 UTF-8
